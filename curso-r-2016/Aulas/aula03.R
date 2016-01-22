2ad29d8d76b0837aae2f723269abd81829ee1d6f
adm <adm@labsoft.local>
2016-01-22 19:47:26 -0200

    atualizacao

---
+++ curso-r-2016/Aulas/aula03.R
@@ -0,0 +1,46 @@
+# Manipulation
+
+library(magrittr)
+library(dplyr)
+library(tidyr)
+
+data(pnud_muni, package = 'abjutils')
+
+
+# example of select
+pnud_muni %>%
+    tbl_df() %>%
+    select(municipio, ufn, ano, gini, starts_with('idhm')) -> x
+
+#  example of filter
+pnud_muni %>%
+    tbl_df() %>%
+    select(municipio, ufn, ano, gini, starts_with('idhm')) %>%
+    filter(ano==2010, gini > 0.5 | idhm > 0.7) -> x
+
+#  example of mutate
+pnud_muni %>%
+    tbl_df() %>%
+    select(municipio, ufn, ano, gini, starts_with('idhm')) %>%
+    filter(ano==2010, gini > 0.5 | idhm > 0.7) %>%
+    mutate(idhm2 = paste(idhm * 100, "%")) -> x
+
+# example of arrange
+pnud_muni %>%
+    tbl_df() %>%
+    select(municipio, ano, idhm) %>%
+    filter(ano==2010) %>%
+    arrange(desc(idhm)) %>%
+    top_n(.,10)
+
+# example of summarise
+# n() count of group_by
+# n_distinct() count of distinct for specified variable
+pnud_muni %>%
+    tbl_df() %>%
+    select(ufn, ano, espvida, popt) %>%
+    filter(ano==2000) %>%
+    group_by(ufn) %>%
+    summarise(mean_lifeexp = sum(espvida*popt)/sum(popt)) %>%
+    arrange(desc(mean_lifeexp))
+
