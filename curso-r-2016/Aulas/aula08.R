659ec4ffe3de6ab9cfa8ff01305b6f157e89bab3
adm <adm@labsoft.local>
2016-02-05 21:01:29 -0200

    update

--- curso-r-2016/Aulas/aula08.R
+++ curso-r-2016/Aulas/aula08.R
@@ -1,5 +1,6 @@
 # Shiny
 library(shiny)
+library(magrittr)
 
 shiny::runGitHub('abjur/vistemplate',
                  subdir='exemplo_01_helloworld',
@@ -8,3 +9,12 @@ shiny::runGitHub('abjur/vistemplate',
 
 # reactive - run once
 # isolate - let choose filter so it can run later
+
+
+diamonds %>%
+    lm(carat ~ price, data = .) %>%
+    coef() -> coef_lm
+
+ggplot(diamonds) +
+    geom_point(aes(x = carat, y = price)) +
+    geom_abline(intercept = coef_lm[1], slope = coef_lm[2])
--- curso-r-2016/Lab8/app.R
+++ curso-r-2016/Lab8/app.R
@@ -15,21 +15,61 @@ library(ggplot2)
 ui <- shinyUI(fluidPage(
 
    # Application title
-   titlePanel("Lab 8"),
+   titlePanel("Lab 8 - Regressão Linear"),
 
    # Sidebar with a slider input for number of bins
    sidebarLayout(
-      sidebarPanel(
+      wellPanel(
+          selectInput(inputId =  "a",
+                      label = "Variavel Resposta",
+                      choices = list(
+                          "carat" = "carat",
+                          "cut" = "cut",
+                          "color" = "color",
+                          "clarity" = "clarity",
+                          "price" = "price"
+                      )
+          ),
+          selectInput(inputId =  "b",
+                      label = "Covariavel",
+                      choices = list(
+                          "carat" = "carat",
+                          "cut" = "cut",
+                          "color" = "color",
+                          "clarity" = "clarity",
+                          "price" = "price"
+                      )
+          )
       ),
 
       # Show a plot of the generated distribution
       mainPanel(
+          fluidRow({
+              plotOutput("id_graf")
+          })
       )
    )
 ))
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
+
+    output$id_graf <- renderPlot({
+        x <- input$b
+        y <- input$a
+
+        lm(diamonds[[y]] ~ diamonds[[x]]) %>%
+            coef() -> coeficiente
+
+        ggplot(diamonds) +
+            geom_point(aes_string(x,y),
+                       color = "dark gray"
+                       ) +
+            geom_abline(intercept = coeficiente[1],
+                        slope = coeficiente[2],
+                        color = "red"
+                        )
+    })
 
 })
 
